5bb538399838b62757051f831cc2b42a4184b719
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-09-28 12:17:05 +0800

    back and fix the issue and the image i hope

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const data = [
   {
     id: 2,
     name: "AMD Ryzen 7 5800X",
-    image: "media/Ryzen 3990x.jpg",
+    image: "media/Ryzen 7 5800x.jpg",
     price: "₱13,280.67",
     rating: "⭐⭐⭐⭐⭐",
   },
